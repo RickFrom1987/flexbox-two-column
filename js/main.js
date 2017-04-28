26599c28d49d0eca82bf2cb21208f12a483170f2
Rick Chen <rick@rickfrom1987.com>
2017-04-27 21:25:42 -0700

    add in basic knockout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("main.js");
+var MOCK_PROJECTS = [
+  { name: 'Project 1', desc: 'Lorem ipsum dolor sitegestas amet vitae' },
+  { name: 'Project 2', desc: 'Lorem ipsum dolor sitegestas amet vitae' },
+  { name: 'Project 3', desc: 'Lorem ipsum dolor sitegestas amet vitae' },
+  { name: 'Project 4', desc: 'Lorem ipsum dolor sitegestas amet vitae' },
+  { name: 'Project 5', desc: 'Lorem ipsum dolor sitegestas amet vitae' },
+  { name: 'Project 6', desc: 'Lorem ipsum dolor sitegestas amet vitae' }
+];
+
+var Demo = {};
+
+Demo.init = function init() {
+  ko.applyBindings({
+    projects: MOCK_PROJECTS
+  });
+}
+
+Demo.init();
